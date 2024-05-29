984ac628d581e689edc6b54d06e358e2d8c74850
HowardHuang <howardhuang122@gmail.com>
2024-05-29 22:40:13 +0800

    add howard week 4

---
+++ 137_Single_Number_ll/136_howard.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun singleNumber(nums: IntArray): Int {
+        val counts = HashMap<Int, Int>()
+        for (num in nums) {
+            counts[num] = counts.getOrDefault(num, 0) + 1
+        }
+        for (num in counts) {
+            if (num.value == 1) {
+                return num.key
+            }
+        }
+    return -1
+    }
+}
---
+++ 217_Contains_Duplicate/217_howard.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun containsDuplicate(nums: IntArray): Boolean {
+         val set = mutableSetOf<Int>()
+        for (num in nums) {
+            if (!set.add(num)) {
+                return true
+            }
+        }
+        return false
+    }
+}
